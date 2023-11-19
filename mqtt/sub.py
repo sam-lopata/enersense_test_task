d8e878ee895e0b49b4a5a8ac7e329aee3b67afdc
Sam <sam.lopata@gmail.com>
2023-11-19 16:28:16 +0200

    make subscriber log into file

--- mqtt/sub.py
+++ mqtt/sub.py
@@ -1,21 +1,30 @@
 """ MQTT subscriber """
 
+import logging
 import paho.mqtt.client as mqtt
+
+# Create a custom logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+f_handler = logging.FileHandler('./log/sub.log', mode='w')
+f_format = logging.Formatter('%(asctime)s  - %(levelname)s - %(message)s')
+f_handler.setLevel(logging.INFO)
+f_handler.setFormatter(f_format)
+logger.addHandler(f_handler)
 
 THE_BROKER = "mqtt"
 THE_TOPIC = "charger/1/connector/1/session/1"
 
 # Callback function used when the client receives a CONNACK response from the broker.
 def on_connect(client, userdata, flags, rc):
-    print("connected to ", client._host, "port: ", client._port)
-    print("flags: ", flags, "returned code: ", rc)
+    logger.info("Connected!")
     client.subscribe(THE_TOPIC, qos=0)
 
 # Callback function used when the client receives a message from the broker.
 def on_message(client, userdata, msg):
-    print("message received with topic: {} and payload: {}".format(
-        msg.topic, str(msg.payload)))
-
+    msg = "Message received with topic: {} and payload: {}".format(msg.topic, str(msg.payload))
+    logger.info(msg)
+    # print(msg)
 
 if __name__ == "__main__":
     client = mqtt.Client()
