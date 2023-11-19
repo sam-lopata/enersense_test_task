85f77adf46171a514dc4eeb2f3eb67ad31e3f378
Sam <sam.lopata@gmail.com>
2023-11-19 18:15:58 +0200

    add save to mongodb, add docker-compose profiles

--- mqtt/sub.py
+++ mqtt/sub.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+
 """ MQTT subscriber """
 
 import logging
 import paho.mqtt.client as mqtt
+import datetime
+from pymongo import MongoClient
 
 # Create a custom logger
 logger = logging.getLogger(__name__)
@@ -11,6 +15,11 @@ f_format = logging.Formatter('%(asctime)s  - %(levelname)s - %(message)s')
 f_handler.setLevel(logging.INFO)
 f_handler.setFormatter(f_format)
 logger.addHandler(f_handler)
+
+# Set up client for MongoDB
+mongo_client=MongoClient(host='mongo', port=27017, username="root", password="password")
+db=mongo_client.enersense_db
+collection=db.enersense_collection
 
 THE_BROKER = "mqtt"
 THE_TOPIC = "charger/1/connector/1/session/1"
@@ -22,8 +31,14 @@ def on_connect(client, userdata, flags, rc):
 
 # Callback function used when the client receives a message from the broker.
 def on_message(client, userdata, msg):
-    msg = "Message received with topic: {} and payload: {}".format(msg.topic, str(msg.payload))
+    topic = msg.topic
+    payload = msg.payload
+    msg = "Message received with topic: {} and payload: {}".format(topic, str(payload))
     logger.info(msg)
+
+    receive_time=datetime.datetime.now()
+    post={"time":receive_time,"topic":topic,"value":str(payload)}
+    collection.insert_one(post)
     # print(msg)
 
 if __name__ == "__main__":
